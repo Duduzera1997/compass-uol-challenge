b82681a6125ce9ecad19f9a096c1fa40c54feaab
Eduardo <eduardojdn1997@gmail.com>
2021-03-21 13:49:00 -0300

    Adiciona mock do async storage.

--- setup-tests.js
+++ setup-tests.js
@@ -1,3 +1,6 @@
+import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import 'jest-styled-components/native';
 
 jest.useFakeTimers();
+
+jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
