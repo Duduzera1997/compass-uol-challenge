a043358451099d98e2d036ab4d69b341008e7e22
Eduardo <eduardojdn1997@gmail.com>
2021-03-20 22:31:47 -0300

    Exporta imagens

---
+++ app/assets/index.js
@@ -0,0 +1,5 @@
+import compassoLogoWhite from './img/logo-white.png';
+import compassoLogoBlack from './img/logo-black.png';
+import nytLogoWhite from './img/nyt-white.png';
+
+export { compassoLogoWhite, compassoLogoBlack, nytLogoWhite };
