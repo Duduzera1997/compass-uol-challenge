433a75a8ad2316ed45166946f3ddeb4b1f43de3e
Eduardo <eduardojdn1997@gmail.com>
2021-03-21 13:47:55 -0300

    Altera configurações do jest, ativa Hermes e troca lib de navegação.

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'react-native',
   collectCoverage: true,
-  collectCoverageFrom: ['src/components/**/*.{js,jsx,ts,tsx}'],
+  collectCoverageFrom: ['app/components/**/*.{js,jsx,ts,tsx}'],
   setupFilesAfterEnv: ['./setup-tests.js'],
   transformIgnorePatterns: ['/node_modules/(?!native-base)/'],
 };
