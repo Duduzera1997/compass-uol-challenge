aa297d85ca77b420c5a539c6eb9f32c6dd255c87
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-22 01:27:29 -0300

    Atualiza componente HeaderLabel com funcionamento.

--- app/components/HeaderLabel/index.js
+++ app/components/HeaderLabel/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-function HeaderLabel() {
+import * as SC from './styles';
+
+function HeaderLabel({ headerText, dateText }) {
   return (
-    <View>
-      <Text>HeaderLabel</Text>
-    </View>
+    <SC.ContainerHeaderLabel>
+      <SC.HeaderTitle>{headerText} News</SC.HeaderTitle>
+      <SC.DateLabel>{dateText}</SC.DateLabel>
+    </SC.ContainerHeaderLabel>
   );
 }
+
+HeaderLabel.defaultProps = {
+  dateText: format(new Date(), "dd 'de' MMMM 'de' yyyy", { locale: ptBR }),
+};
+
+HeaderLabel.propTypes = {
+  headerText: PropTypes.string.isRequired,
+  dateText: PropTypes.string,
+};
 
 export default HeaderLabel;
