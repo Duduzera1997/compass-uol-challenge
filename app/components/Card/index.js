bbd92d62ceaa640764fee526fc2d5dbec6203237
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-22 01:25:48 -0300

    Atualiza componente Card com funcionamento.

--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import * as SC from './styles';
 
-function Card() {
+function Card({ imageUrl, title, abstract, noticeDate }) {
   return (
-    <View>
-      <Text>Card</Text>
-    </View>
+    <SC.ContainerCard>
+      <SC.CardTouchable>
+        <SC.CardContent flex={1}>
+          <SC.PreviewImage source={{ uri: imageUrl }} />
+        </SC.CardContent>
+        <SC.CardContent flex={2.5}>
+          <SC.Text size={15} fontBold>
+            {title}
+          </SC.Text>
+          <SC.TextDate size={11.5} fontBold>
+            {format(new Date(noticeDate), "dd 'de' MMMM 'de' yyyy", {
+              locale: ptBR,
+            })}
+          </SC.TextDate>
+          <SC.Text numberOfLines={2} size={13}>
+            {abstract}
+          </SC.Text>
+        </SC.CardContent>
+      </SC.CardTouchable>
+    </SC.ContainerCard>
   );
 }
+
+Card.propTypes = {
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  abstract: PropTypes.string.isRequired,
+  noticeDate: PropTypes.string.isRequired,
+};
 
 export default Card;
---
+++ app/components/Card/styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components/native';
+
+export const ContainerCard = styled.View`
+  height: 120px;
+  align-items: center;
+  padding-bottom: 10px;
+`;
+
+export const CardTouchable = styled.TouchableOpacity`
+  background-color: ${({ theme }) => theme.colors.secondary.white};
+  width: 92%;
+  height: 100%;
+  border-radius: 8px;
+  flex-direction: row;
+`;
+
+export const CardContent = styled.View`
+  flex: ${({ flex }) => flex};
+  justify-content: flex-start;
+  align-items: center;
+  padding: 5px 5px 2px 7px;
+`;
+
+export const PreviewImage = styled.Image`
+  width: 100%;
+  height: 95%;
+  border-radius: 10px;
+`;
+
+export const Text = styled.Text`
+  width: 99%;
+  font-size: ${({ size }) => size}px;
+  font-family: ${({ theme, fontBold }) =>
+    `${theme.fonts.primary.family}${fontBold ? 'Bold' : 'Regular'}`};
+  color: ${({ theme }) => theme.colors.secondary.darkGrey};
+  padding: 4px 2px 2px 4px;
+`;
+
+export const TextDate = styled(Text)`
+  padding: 0px 2px 2px 4px;
+  color: ${({ theme }) => theme.colors.secondary.grey};
+`;
