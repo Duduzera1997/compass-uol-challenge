0bfe898f5dcfcf48f6652fa5e09b664976d136a9
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-23 19:56:40 -0300

    Altera componente de Loading

--- app/components/Loading/index.js
+++ app/components/Loading/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import { ActivityIndicator } from 'react-native';
-import { ContainerLoading, Logo } from './styles';
+import * as SC from './styles';
 import { compassoLogoWhite as compassoLogo } from '~/assets';
-import { white } from '~/styles/colors';
 
 function Loading() {
   return (
-    <ContainerLoading>
-      <Logo source={compassoLogo} testID="component-loading-logo" />
-      <ActivityIndicator
-        size="small"
-        color={white}
-        testID="component-loading-item"
-      />
-    </ContainerLoading>
+    <SC.ContainerLoading>
+      <SC.Logo source={compassoLogo} testID="component-loading-logo" />
+      <SC.ActivityIndicator testID="component-loading-item" />
+    </SC.ContainerLoading>
   );
 }
 export default Loading;
--- app/components/Loading/styles.js
+++ app/components/Loading/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { white } from '~/styles/colors';
 
 export const ContainerLoading = styled.View`
   flex: 1;
@@ -10,3 +11,8 @@ export const Logo = styled.Image`
   width: 210px;
   height: 47px;
 `;
+
+export const ActivityIndicator = styled.ActivityIndicator.attrs({
+  size: 'small',
+  color: white,
+})``;
