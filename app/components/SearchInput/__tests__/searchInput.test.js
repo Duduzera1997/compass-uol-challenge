57ea1d79aaf6c503f7ab573313ec90f645df5514
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-23 21:27:39 -0300

    Adiciona componente SearchInput

---
+++ app/components/SearchInput/__tests__/searchInput.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SearchInput } from '~/components';
+import { renderWithTheme } from '~/utils/helperTest';
+
+describe('Search Input Component', () => {
+  it('should be contains a "Busque uma notícia" Text', () => {
+    const { getByPlaceholderText, toJSON } = renderWithTheme(<SearchInput />);
+    expect(getByPlaceholderText(/Busque uma notícia/)).toBeTruthy();
+    expect(toJSON()).toMatchSnapshot();
+  });
+});
