2383d791ae92ce266395d283b8153f76cc4906c4
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-23 21:29:35 -0300

    Adiciona Technology Screen.

--- app/views/Technology/__tests__/technology.test.js
+++ app/views/Technology/__tests__/technology.test.js
@@ -1,14 +1,50 @@
 import React from 'react';
-
-import { renderWithStoreProvider } from '~/utils/helperTest';
+import { renderWithStoreAndThemeProviders } from '~/utils/helperTest';
 
 import { TechnologyScreen } from '~/views';
 
+const mock = {
+  section: 'Technology',
+  last_updated: '2021-03-19T17:10:56-04:00',
+  num_results: 1,
+  results: [
+    {
+      title:
+        'For Political Cartoonists, the Irony Was That Facebook Didn’t Recognize Irony',
+      abstract:
+        'As Facebook has become more active at moderating political speech, it has had trouble dealing with satire.',
+      url:
+        'https://www.nytimes.com/2021/03/19/technology/political-cartoonists-facebook-satire-irony.html',
+      uri: 'nyt://article/bc3e4015-4646-590d-86f6-f02038990bf0',
+      byline: 'By Mike Isaac',
+      item_type: 'Article',
+      published_date: '2021-03-22T17:08:27-04:00',
+      multimedia: [
+        {
+          url:
+            'https://static01.nyt.com/images/2021/03/20/business/00fbtoons-print/merlin_182866944_2f787cc6-c290-42ca-a319-ccbf0ae81fc2-articleInline.jpg',
+        },
+      ],
+    },
+  ],
+};
+
 describe('Technology Screen', () => {
-  it('should be contains a Technology text', () => {
-    const { getByText, toJSON } = renderWithStoreProvider(<TechnologyScreen />);
-    const label = getByText('Technology');
-    expect(label).toBeTruthy();
+  it('should be contains a Technology text Label', () => {
+    const state = {
+      data: mock,
+      loading: false,
+      error: false,
+    };
+
+    const { getByText, toJSON } = renderWithStoreAndThemeProviders(
+      <TechnologyScreen />,
+      {
+        techMock: state,
+      },
+    );
+    const component = getByText(/Technology/);
+    expect(component).toBeTruthy();
     expect(toJSON()).toMatchSnapshot();
   });
 });
--- app/views/Technology/index.js
+++ app/views/Technology/index.js
@@ -1,19 +1,91 @@
-import React, { useEffect } from 'react';
-import { View, Text } from 'react-native';
-import { useDispatch } from 'react-redux';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
+
+import * as SC from './styles';
 import { techActions } from '~/redux';
+import {
+  LinearBackground,
+  Header,
+  ListArticles,
+  ViewArticleModal,
+  SearchInput,
+} from '~/components';
 
 function TechnologyScreen() {
+  const { data, loading, error } = useSelector((state) => state.techReducer);
+  const [filterText, setFilterText] = useState('');
+
+  const [modalInfo, setModalInfo] = useState({ visible: false, item: null });
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(techActions.fetchTechnologiesAction());
-  }, [dispatch]);
+    fetchArticlesTechnology();
+  }, [fetchArticlesTechnology]);
+
+  const fetchArticlesTechnology = useCallback(
+    () => dispatch(techActions.fetchTechnologiesAction()),
+    [dispatch],
+  );
+
+  function getFilteredList() {
+    if (_.isEmpty(filterText)) return data;
+    const filteredResults = data.results.filter(
+      (item) =>
+        item.title.toLowerCase().includes(filterText.toLowerCase()) ||
+        item.abstract.toLowerCase().includes(filterText.toLowerCase()),
+    );
+    return {
+      ...data,
+      results: filteredResults,
+      num_results: filteredResults.length,
+    };
+  }
+
+  function onCardPressed(item) {
+    setModalInfo({ ...modalInfo, visible: true, item });
+  }
+
+  function onRequestCloseModal() {
+    setModalInfo({ ...modalInfo, visible: false, item: null });
+  }
+
+  const filteredList = getFilteredList();
 
   return (
-    <View>
-      <Text>Technology</Text>
-    </View>
+    <LinearBackground>
+      <SC.ContainerTech>
+        {modalInfo.visible && (
+          <ViewArticleModal
+            visible={modalInfo.visible}
+            item={modalInfo.item}
+            onRequestClose={onRequestCloseModal}
+          />
+        )}
+        <SC.ContainerByFlex flex={1}>
+          <SC.ContainerByFlex flex={1}>
+            <Header headerText="Technology" />
+          </SC.ContainerByFlex>
+          {!!data?.results?.length && (
+            <SC.ContainerByFlex flex={1}>
+              <SearchInput onChangeText={_.debounce(setFilterText, 500)} />
+            </SC.ContainerByFlex>
+          )}
+          <SC.ContainerByFlex flex={6}>
+            <ListArticles
+              data={filteredList}
+              loading={
+                loading || (!loading && !!filterText && !!filteredList.length)
+              }
+              error={error}
+              onRefresh={fetchArticlesTechnology}
+              onCardPress={onCardPressed}
+            />
+          </SC.ContainerByFlex>
+        </SC.ContainerByFlex>
+      </SC.ContainerTech>
+    </LinearBackground>
   );
 }
+
 export default TechnologyScreen;
