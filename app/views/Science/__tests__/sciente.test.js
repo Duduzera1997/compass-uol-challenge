94ecd70d5b09ed6c03c45242e2a9832df435f332
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-23 21:30:19 -0300

    Adiciona Science Screen.

--- app/views/Science/__tests__/sciente.test.js
+++ app/views/Science/__tests__/sciente.test.js
@@ -1,12 +1,49 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
 
 import { ScienceScreen } from '~/views';
+import { renderWithStoreAndThemeProviders } from '~/utils/helperTest';
+
+const mock = {
+  section: 'Science',
+  last_updated: '2021-03-19T17:10:56-04:00',
+  num_results: 1,
+  results: [
+    {
+      title: 'How to Vaccinate Homebound Seniors? Take the Shots to Them.',
+      abstract:
+        'Millions of older Americans, chronically ill and trapped at home, pose a big challenge to inoculation efforts. Doctors are on it.',
+      url:
+        'https://www.nytimes.com/2021/03/22/health/virus-seniors-vaccination.html',
+      uri: 'nyt://article/e31cffee-6e8f-54eb-95dc-3ab6337fdb18',
+      byline: 'By Roni Caryn Rabin and Chang W. Lee',
+      item_type: 'Article',
+      published_date: '2021-03-22T17:08:27-04:00',
+      multimedia: [
+        {
+          url:
+            'https://static01.nyt.com/images/2021/03/22/science/00VIRUS-HOMEBOUND-promo/00VIRUS-HOMEBOUND-promo-articleInline.jpg',
+        },
+      ],
+    },
+  ],
+};
 
 describe('Science Screen', () => {
-  it('should be contains a Science Text', () => {
-    const { getByText } = render(<ScienceScreen />);
-    const label = getByText('Science');
-    expect(label).toBeTruthy();
+  it('should be contains a Science text Label', () => {
+    const state = {
+      data: mock,
+      loading: false,
+      error: false,
+    };
+
+    const { getByText, toJSON } = renderWithStoreAndThemeProviders(
+      <ScienceScreen />,
+      {
+        scienceMock: state,
+      },
+    );
+    const component = getByText(/Science/);
+    expect(component).toBeTruthy();
+    expect(toJSON()).toMatchSnapshot();
   });
 });
--- app/views/Science/index.js
+++ app/views/Science/index.js
@@ -1,12 +1,89 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import _ from 'lodash';
+
+import * as SC from './styles';
+import { scienceActions } from '~/redux';
+import {
+  LinearBackground,
+  Header,
+  ListArticles,
+  ViewArticleModal,
+  SearchInput,
+} from '~/components';
 
 function ScienceScreen() {
+  const { data, loading, error } = useSelector((state) => state.scienceReducer);
+  const [filterText, setFilterText] = useState('');
+  const [modalInfo, setModalInfo] = useState({ visible: false, item: null });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchArticlesScience();
+  }, [fetchArticlesScience]);
+
+  const fetchArticlesScience = useCallback(
+    () => dispatch(scienceActions.fetchSciencesAction()),
+    [dispatch],
+  );
+
+  function getFilteredList() {
+    if (_.isEmpty(filterText)) return data;
+    const filteredResults = data.results.filter(
+      (item) =>
+        item.title.toLowerCase().includes(filterText.toLowerCase()) ||
+        item.abstract.toLowerCase().includes(filterText.toLowerCase()),
+    );
+    return {
+      ...data,
+      results: filteredResults,
+      num_results: filteredResults.length,
+    };
+  }
+
+  function onCardPressed(item) {
+    setModalInfo({ ...modalInfo, visible: true, item });
+  }
+
+  function onRequestCloseModal() {
+    setModalInfo({ ...modalInfo, visible: false, item: null });
+  }
+
+  const filteredList = getFilteredList();
+
   return (
-    <View>
-      <Text>Science</Text>
-    </View>
+    <LinearBackground>
+      <SC.ContainerScience>
+        {modalInfo.visible && (
+          <ViewArticleModal
+            visible={modalInfo.visible}
+            item={modalInfo.item}
+            onRequestClose={onRequestCloseModal}
+          />
+        )}
+        <SC.ContainerByFlex flex={1}>
+          <SC.ContainerByFlex flex={1}>
+            <Header headerText="Science" />
+          </SC.ContainerByFlex>
+          {!!data?.results?.length && (
+            <SC.ContainerByFlex flex={1}>
+              <SearchInput onChangeText={_.debounce(setFilterText, 500)} />
+            </SC.ContainerByFlex>
+          )}
+          <SC.ContainerByFlex flex={6}>
+            <ListArticles
+              data={filteredList}
+              loading={
+                loading || (!loading && !!filterText && !!filteredList.length)
+              }
+              error={error}
+              onRefresh={fetchArticlesScience}
+              onCardPress={onCardPressed}
+            />
+          </SC.ContainerByFlex>
+        </SC.ContainerByFlex>
+      </SC.ContainerScience>
+    </LinearBackground>
   );
 }
-
 export default ScienceScreen;
