127c4c95fe66238a8251af1277d96e4210fd76d1
Eduardo J. Nascimento <eduardojdn1997@gmail.com>
2021-03-24 10:09:45 -0300

    Remove arquivos desnecessários.

--- app/assets/index.js
+++ app/assets/index.js
@@ -1,5 +1,3 @@
 import compassoLogoWhite from './img/logo-white.png';
-import compassoLogoBlack from './img/logo-black.png';
-import nytLogoWhite from './img/nyt-white.png';
 
-export { compassoLogoWhite, compassoLogoBlack, nytLogoWhite };
+export { compassoLogoWhite };
